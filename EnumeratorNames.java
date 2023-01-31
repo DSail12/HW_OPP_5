ae7b261cf23b7f326a1f8b1db07569281cec6d58
dsail <dsail@yandex.ru>
2023-01-31 10:37:27 +0300

    commit

---
+++ EnumeratorNames.java
@@ -0,0 +1,6 @@
+package OOP_5;
+
+public enum EnumeratorNames {
+    Андрей, Антон, Виктор, Геннадий, Иван, Роман, Сергей, Юрий  
+}
+
---
+++ EnumeratorSurnames.java
@@ -0,0 +1,5 @@
+package OOP_5;
+
+public enum EnumeratorSurnames {
+    Алексеев, Беляев, Иванов, Петров, Костров, Сидоров, Чернов, Щербаков
+}
